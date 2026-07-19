a1e610f2479ae9470b7bd57392e2183c7ac8a0f6
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2026-07-18 22:37:49 -0400

    Add files via upload

--- gropius.c
+++ gropius.c
@@ -334,7 +334,6 @@ signed int SEE(Move* move, GameState* src)
     unsigned char gainsLen = 0;
     unsigned char target;
     signed int capturedPieceVal;
-    char capturingPiece;
     char team;
     Move buffer[_TOTAL_PIECES];
     Move chosenMove;
@@ -346,7 +345,6 @@ signed int SEE(Move* move, GameState* src)
     target = move->to;                                              //  Identify the target.
 
     capturedPieceVal = SEE_SCORE_PAWN;                              //  Value of the captured piece.
-    capturingPiece = gs.board[move->from];
     gains[0] = capturedPieceVal;
     makeMove(move, &gs);                                            //  Apply the move to be evaluated.
     team = gs.blackToMove ? 'b' : 'w';
@@ -372,7 +370,6 @@ signed int SEE(Move* move, GameState* src)
         gainsLen++;
         gains[gainsLen] = victimVal - gains[gainsLen - 1];
 
-        capturingPiece = gs.board[chosenMove.from];                 //  Identify the capturing piece.
         makeMove(&chosenMove, &gs);                                 //  Make the capture.
         team = gs.blackToMove ? 'b' : 'w';
       }
@@ -381,7 +378,7 @@ signed int SEE(Move* move, GameState* src)
       {
         for(j = gainsLen - 1; j >= 0; j--)
           {
-            stopHere = gains[j];                                    //  It is in side-to-move's interest to stop here.
+            stopHere = -gains[j];                                   //  It is in side-to-move's interest to stop here.
             continueExchange = gains[j + 1];                        //  It is in side-to-move's interest to continue exchanging.
             gains[j] = (stopHere > continueExchange) ? -stopHere : -continueExchange;
           }
